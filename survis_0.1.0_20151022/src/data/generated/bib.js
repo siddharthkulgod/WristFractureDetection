2b5274c4c594aa293059b1e244a249ec6d06fb39
Siddharth Kulgod <68397121+siddharthkulgod@users.noreply.github.com>
2023-05-15 20:13:43 +0530

    Add files via upload

---
+++ survis_0.1.0_20151022/src/data/generated/bib.js
@@ -0,0 +1,127 @@
+﻿define({ entries : {
+    "Beck2016Visual": {
+        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
+        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
+        "volume": "22",
+        "year": "2016"
+    },
+    "bluthgen2020detection": {
+        "author": "Bl{\\\"u}thgen, Christian and Becker, Anton S and de Martini, Ilaria Vittoria and Meier, Andreas and Martini, Katharina and Frauenfelder, Thomas",
+        "journal": "European journal of radiology",
+        "pages": "108925",
+        "publisher": "Elsevier",
+        "title": "Detection and localization of distal radius fractures: Deep learning system versus radiologists",
+        "type": "article",
+        "volume": "126",
+        "year": "2020"
+    },
+    "ebsim2019automatic": {
+        "author": "Ebsim, Raja and Naqvi, Jawad and Cootes, Timothy F",
+        "booktitle": "Computational Methods and Clinical Applications in Musculoskeletal Imaging: 6th International Workshop, MSKI 2018, Held in Conjunction with MICCAI 2018, Granada, Spain, September 16, 2018, Revised Selected Papers 6",
+        "organization": "Springer",
+        "pages": "114--125",
+        "title": "Automatic detection of wrist fractures from posteroanterior and lateral radiographs: a deep learning-based approach",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "hardalacc2022fracture": {
+        "author": "Hardala{\\c{c} F{\\i}rat and Uysal, Fatih and Peker, Ozan and {\\c{C}}i{\\c{c}}eklida{\\u{g} Murat and Tolunay, Tolga and Tokg{\\\"o}z, Nil and Kutbay, U{\\u{g}}urhan and Demirciler, Boran and Mert, Fatih",
+        "journal": "Sensors",
+        "number": "3",
+        "pages": "1285",
+        "publisher": "MDPI",
+        "title": "Fracture detection in wrist X-ray images using deep learning-based object detection models",
+        "type": "article",
+        "volume": "22",
+        "year": "2022"
+    },
+    "hrvzic2022fracture": {
+        "author": "Hr{\\v{z}}i{\\'c Franko and Tschauner, Sebastian and Sorantin, Erich and {\\v{S}}tajduhar, Ivan",
+        "journal": "Mathematics",
+        "number": "16",
+        "pages": "2939",
+        "publisher": "MDPI",
+        "title": "Fracture Recognition in Paediatric Wrist Radiographs: An Object Detection Approach",
+        "type": "article",
+        "volume": "10",
+        "year": "2022"
+    },
+    "kim2018artificial": {
+        "author": "Kim, DH and MacKinnon, T",
+        "journal": "Clinical radiology",
+        "number": "5",
+        "pages": "439--445",
+        "publisher": "Elsevier",
+        "title": "Artificial intelligence in fracture detection: transfer learning from deep convolutional neural networks",
+        "type": "article",
+        "volume": "73",
+        "year": "2018"
+    },
+    "kim2019automatic": {
+        "abstract": "Early detection of subtle fracture is important particularly for the senior citizens\u2019 quality of life. Naked eye examination from X-ray image may cause false negatives due to operator subjectivity thus computer vision based automatic detection software is much needed in practice. In this paper, we propose an automatic extraction method for suspicious wrist fracture regions. We apply K-means in pixel clustering to form the candidate part of possible fracture from wrist X-ray image automatically. This method can recover previously detected patterned false cases with edge detection method after fuzzy stretching. The proposed method is successful in 16 out of 20 tested cases in experiment",
+        "author": "Kim, Kwang Baek and Song, Doo Heon and Yun, Sang-Seok",
+        "journal": "International Journal of Electrical and Computer Engineering",
+        "number": "6",
+        "pages": "5205",
+        "publisher": "IAES Institute of Advanced Engineering and Science",
+        "title": "Automatic segmentation of wrist bone fracture area by K-means pixel clustering from X-ray image",
+        "type": "article",
+        "volume": "9",
+        "year": "2019"
+    },
+    "lindsey2018deep": {
+        "abstract": "Suspected fractures are among the most common reasons for patients to visit emergency departments (EDs), and X-ray imaging is the primary diagnostic tool used by clinicians to assess patients for fractures. Missing a fracture in a radiograph often has severe consequences for patients, resulting in delayed treatment and poor recovery of function. Nevertheless, radiographs in emergency settings are often read out of necessity by emergency medicine clinicians who lack subspecialized expertise in orthopedics, and misdiagnosed fractures account for upward of four of every five reported diagnostic errors in certain EDs. In this work, we developed a deep neural network to detect and localize fractures in radiographs. We trained it to accurately emulate the expertise of 18 senior subspecialized orthopedic surgeons by having them annotate 135,409 radiographs. We then ran a controlled experiment with emergency medicine clinicians to evaluate their ability to detect fractures in wrist radiographs with and without the assistance of the deep learning model. The average clinician\u2019s sensitivity was 80.8% (95% CI, 76.7\u201384.1%) unaided and 91.5% (95% CI, 89.3\u201392.9%) aided, and specificity was 87.5% (95 CI, 85.3\u201389.5%) unaided and 93.9% (95% CI, 92.9\u201394.9%) aided. The average clinician experienced a relative reduction in misinterpretation rate of 47.0% (95% CI, 37.4\u201353.9%). The significant improvements in diagnostic accuracy that we observed in this study show that deep learning methods are a mechanism by which senior medical specialists can deliver their expertise to generalists on the front lines of medicine, thereby providing substantial improvements to patient care.",
+        "author": "Lindsey, Robert and Daluiski, Aaron and Chopra, Sumit and Lachapelle, Alexander and Mozer, Michael and Sicular, Serge and Hanel, Douglas and Gardner, Michael and Gupta, Anurag and Hotchkiss, Robert and others",
+        "journal": "Proceedings of the National Academy of Sciences",
+        "number": "45",
+        "pages": "11591--11596",
+        "publisher": "National Acad Sciences",
+        "title": "Deep neural network improves fracture detection by clinicians",
+        "type": "article",
+        "volume": "115",
+        "year": "2018"
+    },
+    "raisuddin2021critical": {
+        "abstract": "Wrist Fracture is the most common type of fracture with a high incidence rate. Conventional radiography (i.e. X-ray imaging) is used for wrist fracture detection routinely, but occasionally fracture delineation poses issues and an additional confirmation by computed tomography (CT) is needed for diagnosis. Recent advances in the field of Deep Learning (DL), a subfield of Artificial Intelligence (AI), have shown that wrist fracture detection can be automated using Convolutional Neural Networks. However, previous studies did not pay close attention to the difficult cases which can only be confirmed via CT imaging. In this study, we have developed and analyzed a state-of-the-art DL-based pipeline for wrist (distal radius) fracture detection\u2014DeepWrist, and evaluated it against one general population test set, and one challenging test set comprising only cases requiring confirmation by CT. Our results reveal that a typical state-of-the-art approach, such as DeepWrist, while having a near-perfect performance on the general independent test set, has a substantially lower performance on the challenging test set\u2014average precision of 0.99 (0.99\u20130.99) versus 0.64 (0.46\u20130.83), respectively. Similarly, the area under the ROC curve was of 0.99 (0.98\u20130.99) versus 0.84 (0.72\u20130.93), respectively. Our findings highlight the importance of a meticulous analysis of DL-based models before clinical use, and unearth the need for more challenging settings for testing medical AI systems.",
+        "author": "Raisuddin, Abu Mohammed and Vaattovaara, Elias and Nevalainen, Mika and Nikki, Marko and J{\\\"a}rvenp{\\\"a}{\\\"a Elina and Makkonen, Kaisa and Pinola, Pekka and Palsio, Tuula and Niemensivu, Arttu and Tervonen, Osmo and others",
+        "journal": "Scientific reports",
+        "number": "1",
+        "pages": "1--11",
+        "publisher": "Springer",
+        "title": "Critical evaluation of deep neural networks for wrist fracture detection",
+        "type": "article",
+        "volume": "11",
+        "year": "2021"
+    },
+    "rashid2023minority": {
+        "author": "Rashid, Tooba and Zia, Muhammad Sultan and Meraj, Talha and Rauf, Hafiz Tayyab and Kadry, Seifedine",
+        "journal": "Life",
+        "number": "1",
+        "pages": "133",
+        "publisher": "MDPI",
+        "title": "A Minority Class Balanced Approach Using the DCNN-LSTM Method to Detect Human Wrist Fracture",
+        "type": "article",
+        "volume": "13",
+        "year": "2023"
+    },
+    "thian2019convolutional": {
+        "author": "Thian, Yee Liang and Li, Yiting and Jagmohan, Pooja and Sia, David and Chan, Vincent Ern Yao and Tan, Robby T",
+        "journal": "Radiology: Artificial Intelligence",
+        "number": "1",
+        "pages": "e180001",
+        "publisher": "Radiological Society of North America",
+        "title": "Convolutional neural networks for automated fracture detection and localization on wrist radiographs",
+        "type": "article",
+        "volume": "1",
+        "year": "2019"
+    }
+}});
